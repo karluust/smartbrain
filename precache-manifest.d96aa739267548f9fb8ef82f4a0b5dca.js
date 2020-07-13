0a13ee551c3cbe591b75b7f37e35f89799d5b9e5
Karl Martin <KarlMartin@DESKTOP-ECPCQRP>
2020-07-13 21:40:31 +0300

    Updates

---
+++ precache-manifest.d96aa739267548f9fb8ef82f4a0b5dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35eadb4dc1d0b488db8ae8e5f03b2ea6",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "033fbb654ddfcb9b14d1",
+    "url": "/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cb04e953693444268f13",
+    "url": "/smartbrain/static/css/main.9f786944.chunk.css"
+  },
+  {
+    "revision": "033fbb654ddfcb9b14d1",
+    "url": "/smartbrain/static/js/2.f88ec925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain/static/js/2.f88ec925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb04e953693444268f13",
+    "url": "/smartbrain/static/js/main.9c69d8f9.chunk.js"
+  },
+  {
+    "revision": "ae456e4515232efad5b0",
+    "url": "/smartbrain/static/js/runtime-main.b715ec47.js"
+  }
+]);
